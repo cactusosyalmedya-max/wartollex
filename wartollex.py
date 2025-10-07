7272a9294ff4cd27cae615cdee49f8862df3bd01
cactusosyalmedya-max <cactusosyalmedya@gmail.com>
2025-10-07 14:18:17 +0300

    Update wartollex.py

--- wartollex.py
+++ wartollex.py
@@ -1,1 +1,8 @@
+import time
 
+print("✅ Wartollex bot başlatıldı!")
+print("Bot çalışıyor... Render bağlantısı başarılı 🔥")
+
+# Şimdilik sadece çalışır durumda kalsın
+while True:
+    time.sleep(60)
